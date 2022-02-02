7c5860d2684439a2603d7ca53474f3c927486fbd
AhmadMurad1 <ahmadmur@post.bgu.ac.il>
2022-02-02 11:53:06 +0200

    add code

--- feature_detect.m
+++ feature_detect.m
@@ -1,4 +1,10 @@
-function [BB, mask, score] = feature_detect(gray)
-
+function [bboxes, detectedImg, scores] = feature_detect(gray)
+    name = 'tiny-yolov3-coco';
+    detector = yolov3ObjectDetector(name);
+    analyzeNetwork(detector.Network);
+    img = preprocess(detector,gray);
+    img = im2single(img);
+    [bboxes,scores,labels] = detect(detector,img,'DetectionPreprocessing','none');
+    detectedImg = insertObjectAnnotation(img,'Rectangle',bboxes,labels);
 
 end
