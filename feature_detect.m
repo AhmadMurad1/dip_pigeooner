ab42bbbb65521bc84bdb6a9872db097f1d9bae2c
AhmadMurad1 <ahmadmur@post.bgu.ac.il>
2022-02-06 10:13:57 +0200

    add code

--- feature_detect.m
+++ feature_detect.m
@@ -116,7 +116,47 @@ function [boxes, detectedImg, scores] = feature_detect(gray)
     figure(3)
     imagesc(contain);
 
-
+    %put object containing box coordinates and other relevant information in a cell
+    %array
+    counter = 0;
+    for i = 1:7
+        for j = 1:7
+            if contain(i,j) == 1
+                counter = counter+1;           
+                
+                % Bounding box center relative to cell
+                x = outArray(i,j,22+1+(cellIndex(i,j)-1)*4);
+                y = outArray(i,j,22+2+(cellIndex(i,j)-1)*4);
+                
+                % Yolo outputs the square root of the width and height of the
+                % bounding boxes (subtle detail in paper that took me forver to realize). 
+                % Relative to image size.
+                w = (outArray(i,j,22+3+(cellIndex(i,j)-1)*4))^2;
+                h = (outArray(i,j,22+4+(cellIndex(i,j)-1)*4))^2;
+            
+            %absolute values scaled to image size
+                wS = w*448; 
+                hS = h*448;
+                xS = (j-1)*448/7+x*448/7-wS/2;
+                yS = (i-1)*448/7+y*448/7-hS/2;
+                
+                % this array will be used for drawing bounding boxes in Matlab
+                boxes(counter).coords = [xS yS wS hS]; 
+                
+                %save cell indices in the structure
+                boxes(counter).cellIndex = [i,j];
+                
+                %save classIndex to structure
+                boxes(counter).classIndex = classMaxIndex(i,j);    
+                
+                % save cell proability to structure
+                boxes(counter).cellProb = cellProb(i,j);
+                
+                % put in a switch for non max which we will use later
+                boxes(counter).nonMax = 1;
+            end            
+        end
+    end
     % Begin non max suppression. If intersection over union of any two bounding boxes is 
     % higher than a defined threshold, and the two boxes contain the same class,
     % remove the box with the the lower box probability.
